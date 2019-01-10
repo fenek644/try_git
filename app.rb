69295a72eb1b2d91d80773a4d07ade29fa5290e1
fenek644 <fenek644@ya.ru>
2019-01-10 22:03:22 +0300

    Step 2 : add an object

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ class Cl
   def self.a
     puts "I'm from class 'Cl', function a"
   end
+  def a
+    puts "I'm function a from object of the class 'Cl'"
+  end
 end
 
+
+
 Cl.a
+
+obj = Cl.new
+
+obj.a
